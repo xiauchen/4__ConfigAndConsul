9a36ab023b52bff4298651e9731c825b4fa8f673
moonbt04 <qingxiao4@gmail.com>
2020-01-21 16:14:37 +0800

    gitlab private network problem

---
+++ 4-6-1__RabbitHello/src/main/java/com/example/__RabbitHello/RabbitConfig.java
@@ -0,0 +1,17 @@
+//package com.example.__RabbitHello;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//@Configuration
+//public class RabbitConfig {
+//    @Bean
+//    public Queue helloQueue(){
+//        Queue q = new LinkedList<String>();
+//        q.add("hello");
+//        return q;
+//    }
+//}
---
+++ 4-6-1__RabbitHello/src/main/java/com/example/__RabbitHello/Receiver.java
@@ -0,0 +1,12 @@
+//package com.example.__RabbitHello;
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Receiver {
+//        @RabbitHandler
+//        public void process(String hello){
+//            System.out.println("Receiver : " + hello);
+//        }
+//}

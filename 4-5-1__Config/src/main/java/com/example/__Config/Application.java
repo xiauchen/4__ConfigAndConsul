f6acdb2c53a66e3832ce7c6bd693bac1c5bf2f7c
moonbt04 <qingxiao4@gmail.com>
2020-02-15 18:23:53 +0800

    config merge eureka

--- 4-5-1__Config/src/main/java/com/example/__Config/Application.java
+++ 4-5-1__Config/src/main/java/com/example/__Config/Application.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaServer
 public class Application {
 
 	public static void main(String[] args) {
